e3ea7cc5028d173e7b4bc7b758919ab8fffb185f
Ilya Pugachev <ilyapugachv@gmail.com>
2023-08-24 14:25:06 +0300

    CreateAccountViewController completed

---
+++ homeWork#15/homeWork#15/Controllers/CreateAccountViewController.swift
@@ -0,0 +1,69 @@
+//
+//  CreateAccountViewController.swift
+//  homeWork#15
+//
+//  Created by Илья Пугачёв on 24.08.23.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    // Enter your email
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var errorEmailLbl: UILabel!
+    
+    // Enter your name
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    // Create strong password
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var errorPasswrodLbl: UILabel!
+    
+    // strongPassIndicatorsViews
+    @IBOutlet var strongPassIndicatorsViews: [UIView]!
+    
+    // confirmPassTextField
+    @IBOutlet weak var confirmPassTextField: UITextField!
+    @IBOutlet weak var errorConfirmPassLbl: UILabel!
+    
+    // continueButton
+    @IBOutlet weak var continueButton: UIButton!
+    
+    // scrollView
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
