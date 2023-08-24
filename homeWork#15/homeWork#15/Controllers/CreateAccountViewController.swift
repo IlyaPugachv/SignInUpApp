43b0f9167f55c4c61e6cd74d65a0b730c923147e
Ilya Pugachev <ilyapugachv@gmail.com>
2023-08-24 15:02:21 +0300

    SignInUp App. Part 1

--- homeWork#15/homeWork#15/Controllers/CreateAccountViewController.swift
+++ homeWork#15/homeWork#15/Controllers/CreateAccountViewController.swift
@@ -35,14 +35,31 @@ class CreateAccountViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        strongPassIndicatorsViews.forEach { view in view.alpha = 0.2 }
+        hideKeyboardWhenTappedAround()
+        startKeyboardObserver()
     }
     
 
+    private func startKeyboardObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
     
+    @objc private func keyboardWillShow(notification: Notification) {
+        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize.height, right: 0.0)
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+    }
     
-    
-    
-    
+    @objc private func keyboardWillHide() {
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: 0.0)
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+    }
     
     
     
